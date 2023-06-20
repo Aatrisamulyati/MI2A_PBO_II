8602cc7f4219263b0ea3fd8bb8dc1fbb6d297235
Aatrisamulyati <114573287+Aatrisamulyati@users.noreply.github.com>
2023-06-20 23:54:48 +0700

    Add files via upload

---
+++ src/helper/DosenData.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helper;
+
+/**
+ *
+ * @author hp
+ */
+public class DosenData {
+    public static final String [][] data={
+        {"123456678695","Aatrisa Mulyati","P","2011"},
+        {"735929565685","Berliana Dewinta Martin","P","2003"},
+        {"503478287684","Ratih Yudistiviana","P","2005"},
+        {"935054865865","Dea Insani Putri","P","2007"},
+        
+    };
+    
+    public static final String [] TABLE_HEADER=
+    {"nip","nama","gender","angkatan"};
+}
+
+
---
+++ src/helper/DummyData.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helper;
+
+/**
+ *
+ * @author hp
+ */
+public class DummyData {
+    public static final String [][] data={
+        {"2101092024","Aatrisa Mulyati","P"},
+        {"2101092003","Berliana Dewinta Martin","P"},
+        {"2101092065","Ratih Yudistiviana","P"},
+        {"2101092042","Dea Insani Putri","P"},
+        
+    };
+    
+    public static final String [] TABLE_HEADER=
+    {"nobp","nama","gender"};
+}
